e1226cc5ef7961e57ef7dec3a642bab6436abd27
EmiTis Yousefi <emitis89@yahoo.com>
2019-05-27 23:46:37 +0430

    add service provider

---
+++ src/WhiteHouseResponder/Laravel/WhiteHouseResponderServiceProvider.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emitis
+ * Date: 5/27/19
+ * Time: 11:02 PM
+ */
+
+namespace WhiteHouseResponder\Laravel;
+
+
+use Illuminate\Support\ServiceProvider;
+use WhiteHouseResponder\ErrorsRepository;
+use WhiteHouseResponder\Response;
+
+class WhiteHouseResponderServiceProvider extends ServiceProvider
+{
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        $this->registerSingletons();
+    }
+
+
+
+    /**
+     * Registers the singleton.
+     */
+    protected function registerSingletons()
+    {
+        $this->registerResponse();
+        $this->registerErrorsSingleton();
+    }
+
+
+
+    /**
+     * Registers the singleton for the response.
+     */
+    protected function registerResponse()
+    {
+        $this->app->singleton('whitehouse.response', function () {
+            return app()->make(Response::class);
+        });
+    }
+
+
+
+    /**
+     * Registers the singleton for the response.
+     */
+    protected function registerErrorsSingleton()
+    {
+        $this->app->singleton('whitehouse.errors', function ($app) {
+            return new ErrorsRepository();
+        });
+
+        $this
+             ->app
+             ->when(Response::class)
+             ->needs(ErrorsRepository::class)
+             ->give(function () {
+                 return app('whitehouse.errors');
+             })
+        ;
+    }
+}
